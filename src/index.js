2ed51e8ef24e2faf4bb241684e312249c86ddc40
t-skgm <skgm0301@gmail.com>
2018-02-12 20:24:36 +0900

    s02.03

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types'
 const createReactClass = require('create-react-class');
 
 const Component = createReactClass({
+  propTypes: {
+    name: PropTypes.string.isRequired
+  },
   render: function() {
     return <span>I'm { this.props.name }.</span>;
   }
@@ -10,7 +14,7 @@ const Component = createReactClass({
 
 ReactDOM.render(
   React.createElement(Component, {
-    name: "Bob"
+    // name: "Bob"
   }),
   document.getElementById("app")
 );
