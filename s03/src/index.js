d03f7b572f9f2fbacaf0593a5c66c1177e312243
t-skgm <skgm0301@gmail.com>
2018-03-08 17:01:52 +0900

    s3 initialize

---
+++ s03/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types'
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+const createReactClass = require('create-react-class');
